c4c9b2e0e43f5feaef5b2821a2c4ecbf9314e127
toshko <toshkooooooooooo@gmail.com>
2015-03-25 09:51:30 +0200

    Adding IUnit interface

--- StarCraft/StarCraft/GameObject/Unit.cs
+++ StarCraft/StarCraft/GameObject/Unit.cs
@@ -1,11 +1,22 @@
 ﻿namespace StarCraft.GameObject
 {
-   public abstract class Unit : MovingObject
+    using StarCraft.Interfaces;
+   public abstract class Unit : MovingObject, IUnit
     {
        public Unit(Position position, string owner, int health)
            : base(position, owner, health)
        {
 
+       }
+
+       public RaceType Race
+       {
+           get { throw new System.NotImplementedException(); }
+       }
+
+       public UnitType Type
+       {
+           get { throw new System.NotImplementedException(); }
        }
     }
 }
