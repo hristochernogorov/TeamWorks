c1f43237d7b98404253b47a0a2afa0cb1e3d6652
toshko <toshkooooooooooo@gmail.com>
2015-04-03 09:32:41 +0300

    Add IUnitFactory interface

---
+++ StarCraft/StarCraft/Interfaces/IUnitFactory.cs
@@ -0,0 +1,10 @@
+﻿
+namespace StarCraft.Interfaces
+{
+    using StarCraft.GameObject;
+    interface IUnitFactory
+    {
+        IFighter CreateFighterUnit(Position position, IPlayer player, int mineral, int gas, int sloat);
+        IGatherer CreateGathererUnit(Position position, IPlayer player, int mineral, int gas, int sloat);
+    }
+}
