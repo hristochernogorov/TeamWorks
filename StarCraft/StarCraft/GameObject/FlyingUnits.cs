e882590180f5565e62c43602cb66093174dac775
TomaNikolov <on.tomas@gmail.com>
2015-04-03 21:55:13 +0300

    add new class

---
+++ StarCraft/StarCraft/GameObject/FlyingUnits.cs
@@ -0,0 +1,41 @@
+﻿namespace StarCraft.GameObject
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Interfaces;
+
+    public class FlyingUnits : Unit
+    {
+        public FlyingUnits(Position position, string name, int mineralCost, int gasCost, int sloat, int health, int attackDmg, RaceType race, UnitType unitType = UnitType.Pedestrian)
+            : base(position, name, mineralCost, gasCost, sloat, health, attackDmg, race, UnitType.Flying)
+        {
+
+        }
+
+        public override void ResponseToAttackCommand(IGameObject obj)
+        {
+            this.GoTo(obj.Position);
+            this.Attack(obj);
+        }
+
+        public override void FindObjectToAttack(IEnumerable<IGameObject> opsitePlayerUnitOnSamePossition)
+        {
+            var objectToAtaack = opsitePlayerUnitOnSamePossition.FirstOrDefault(o => (o is Unit));
+            if (objectToAtaack != null)
+            {
+                this.Attack(objectToAtaack);
+            }
+            else
+            {
+                objectToAtaack = opsitePlayerUnitOnSamePossition.FirstOrDefault(o => o is Building);
+                if (objectToAtaack != null)
+                {
+                    this.Attack(objectToAtaack);
+                }
+
+            }
+        }
+
+
+    }
+}
---
+++ StarCraft/StarCraft/GameObject/Gatherer.cs
@@ -0,0 +1,29 @@
+﻿namespace StarCraft.GameObject
+{
+    using Interfaces;
+
+    public class Gatherer : Unit, IGatherer
+    {
+        private ResourceType collectedResourceType;
+
+        public Gatherer(Position position, string name, RaceType race)
+            : base(position, name, 10, 0, 1, 40, 2, race)
+        {
+        }
+
+        public bool CollectResources(ResourceType resource)
+        {
+            return this.CollectedResource == resource;
+        }
+
+        public void Work(ResourceType resource)
+        {
+            this.collectedResourceType = resource;
+        }
+
+        public ResourceType CollectedResource
+        {
+            get { return this.collectedResourceType; }
+        }
+    }
+}
