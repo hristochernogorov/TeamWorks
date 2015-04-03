36c1b8d1101c25999440613c30ed8691fd0bc580
toshko <toshkooooooooooo@gmail.com>
2015-04-03 09:37:58 +0300

    Add UnitFactory class

---
+++ StarCraft/StarCraft/Engine/UnitFactory.cs
@@ -0,0 +1,20 @@
+﻿
+namespace StarCraft.Engine
+{
+    using StarCraft.GameObject;
+    using StarCraft.Interfaces;
+    using StarCraft.CustomAttributes;
+    [Author("Todor Dimitrov")]
+    public class UnitFactory : IUnitFactory
+    {
+        public IFighter CreateFighterUnit(Position position, IPlayer player, int mineral, int gas, int sloat)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IGatherer CreateGathererUnit(Position position, IPlayer player, int mineral, int gas, int sloat)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- StarCraft/StarCraft/Interfaces/IUnitFactory.cs
+++ StarCraft/StarCraft/Interfaces/IUnitFactory.cs
@@ -2,6 +2,8 @@
 namespace StarCraft.Interfaces
 {
     using StarCraft.GameObject;
+    using StarCraft.CustomAttributes;
+    [Author("Todor Dimitrov")]
     interface IUnitFactory
     {
         IFighter CreateFighterUnit(Position position, IPlayer player, int mineral, int gas, int sloat);
