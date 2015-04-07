778d9f892c82f99ac9005e01c9441e12898d55ea
toshko <toshkooooooooooo@gmail.com>
2015-04-07 09:57:33 +0300

    adding BuildingFactory class

--- StarCraft/StarCraft/GameObject/Buildings/Races/Protoss/Gateway.cs
+++ StarCraft/StarCraft/GameObject/Buildings/Races/Protoss/Gateway.cs
@@ -6,10 +6,10 @@
     {
         private const int ProtossGatewayMineralCost = 100;
         private const int ProtossGatewayGasCost = 20;
-        private const int ProtossGatewaygHealth = 500;
+        private const int ProtossGatewayHealth = 500;
 
         public Gateway(Position position, string name)
-            : base(position, name, ProtossGatewayMineralCost, ProtossGatewayGasCost, ProtossGatewaygHealth, RaceType.Protoss)
+            : base(position, name, ProtossGatewayMineralCost, ProtossGatewayGasCost, ProtossGatewayHealth, RaceType.Protoss)
         {
         }
 
--- StarCraft/StarCraft/GameObject/Buildings/Races/Zerg/SpawningPool.cs
+++ StarCraft/StarCraft/GameObject/Buildings/Races/Zerg/SpawningPool.cs
@@ -8,7 +8,7 @@
         private const int ZergSpawningPoolGasCost = 20;
         private const int ZergSpawningPoolyHealth = 750;
 
-        public SpawningPool(Position position, string name, int sloat)
+        public SpawningPool(Position position, string name)
             : base(position, name, ZergSpawningPoolMineralCost, ZergSpawningPoolGasCost, ZergSpawningPoolyHealth, RaceType.Zerg)
         {
         }
